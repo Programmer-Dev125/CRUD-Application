2bfa8a0a1e84c74f5a4ffa3809a9641cca1fda4e
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-12 08:48:50 -0500

    c

--- api/mongo.js
+++ api/mongo.js
@@ -27,131 +27,74 @@ export default async function handleServer(req, res) {
     "content-type, x-user-id, authorization"
   );
 
-  try {
-    switch (req.method) {
-      case "OPTIONS":
+  switch (req.method) {
+    case "OPTIONS":
+      res.writeHead(200);
+      res.end();
+      break;
+    case "GET":
+      const data = await isModel.find({}, { _id: 0, __v: 0 });
+      if (Array.isArray(data)) {
         res.writeHead(200);
-        return res.end();
-
-      case "GET":
-        const data = await isModel.find({}, { _id: 0, __v: 0 });
-        if (Array.isArray(data)) {
-          res.writeHead(200);
-          res.end(JSON.stringify(data));
-        } else {
-          res.writeHead(500);
-          res.end(JSON.stringify({ error: "server failed to fetch data" }));
+        res.end(JSON.stringify(data));
+      } else {
+        res.writeHead(500);
+        res.end(JSON.stringify({ error: "server failed to fetch data" }));
+      }
+      break;
+    case "POST":
+      let isPostBody = "";
+      req.on("data", (data) => {
+        isPostBody += data;
+      });
+      req.on("end", () => {
+        const isObj = JSON.parse(isPostBody);
+        if (!Object.hasOwn("isObj", "name")) {
+          res.writeHead(400);
+          return res.end(JSON.stringify({ error: "Missing Request body" }));
         }
-        break;
-      case "POST":
-        let isPostBody = "";
-        req.on("data", (data) => {
-          isPostBody += data;
-        });
-        req.on("end", () => {
-          if (!isPostBody) {
-            res.writeHead(400);
-            return res.end(JSON.stringify({ error: "Missing Request body" }));
-          }
-          (async () => {
-            const isObj = JSON.parse(isPostBody);
-            const hasNameExist = await isModel.findOne({
-              $or: [
-                { name: { $regex: new RegExp(`^${isObj.name}`, "i") } },
-                { email: { $regex: new RegExp(`^${isObj.email}`, "i") } },
-              ],
-            });
-            if (hasNameExist) {
-              res.writeHead(409);
-              return res.end(
-                JSON.stringify({
-                  error: "name or email already exists, change either of them",
-                })
-              );
-            }
-            const lastId = await isModel.findOne().sort({ _id: -1 }).lean();
-            const isId = lastId?.id ? lastId + 1 : 1;
-            const toCreate = await isModel.create(
-              {
-                id: isId,
-                name: isObj.name,
-                age: parseInt(isObj.age),
-                email: isObj.email,
-              },
-              { ordered: true }
+
+        (async () => {
+          const hasNameExist = await isModel.findOne({
+            $or: [
+              { name: { $regex: new RegExp(`^${isObj.name}`, "i") } },
+              { email: { $regex: new RegExp(`^${isObj.email}`, "i") } },
+            ],
+          });
+          if (!Object.hasOwn(hasNameExist, "name")) {
+            res.writeHead(409);
+            return res.end(
+              JSON.stringify({
+                error: "name or email already exists, change either of them",
+              })
             );
-            if (!toCreate) {
-              res.writeHead(400);
-              return res.end(
-                JSON.stringify({ error: "Error creating resource" })
-              );
-            }
-            res.writeHead(201);
-            return res.end(JSON.stringify({ success: "The user is created" }));
-          })();
-        });
-        break;
+          }
+          const lastId = await isModel.findOne().sort({ _id: -1 }).lean();
+          const isId = lastId?.id ? lastId + 1 : 1;
+          const toCreate = await isModel.create(
+            {
+              id: isId,
+              name: isObj.name,
+              age: parseInt(isObj.age),
+              email: isObj.email,
+            },
+            { ordered: true }
+          );
+          if (!Object.hasOwn(toCreate, "name")) {
+            res.writeHead(400);
+            return res.end(
+              JSON.stringify({ error: "Error creating resource" })
+            );
+          }
+          res.writeHead(201);
+          return res.end(JSON.stringify({ success: "The user is created" }));
+        })();
+      });
+      break;
 
-      // case "PUT":
-      //   const putId = parseInt(req.headers["x-user-id"]);
-      //   let putData = "";
-      //   req.on("data", (chunk) => {
-      //     putData += chunk;
-      //   });
-
-      //   req.on("end", async () => {
-      //     try {
-      //       if (!putData) {
-      //         res.writeHead(400);
-      //         return res.end(JSON.stringify({ error: "Empty request body" }));
-      //       }
-
-      //       const isPutObj = JSON.parse(putData);
-      //       const toUpdate = await isModel.updateOne(
-      //         { id: putId },
-      //         {
-      //           name: isPutObj.name,
-      //           age: parseInt(isPutObj.age),
-      //           email: isPutObj.email,
-      //         }
-      //       );
-
-      //       if (toUpdate.modifiedCount === 1) {
-      //         res.writeHead(200);
-      //         res.end(
-      //           JSON.stringify({ success: "The user credentials are updated" })
-      //         );
-      //       } else {
-      //         res.writeHead(400);
-      //         res.end(
-      //           JSON.stringify({ error: "Error updating the user credentials" })
-      //         );
-      //       }
-      //     } catch (err) {
-      //       res.writeHead(400);
-      //       res.end(JSON.stringify({ error: "Invalid JSON format" }));
-      //     }
-      //   });
-      //   break;
-
-      // case "DELETE":
-      //   const delId = parseInt(req.headers["x-del-id"]);
-      //   const hasDelete = await isModel.deleteOne({ id: delId });
-      //   if (hasDelete.deletedCount === 1) {
-      //     res.writeHead(200);
-      //     res.end(JSON.stringify({ success: "The user has been deleted" }));
-      //   } else {
-      //     res.writeHead(400);
-      //     res.end(JSON.stringify({ error: "Error deleting the user" }));
-      //   }
-      //   break;
-
-      default:
-        res.writeHead(405);
-        res.end(JSON.stringify({ error: "Method not allowed" }));
-    }
-  } catch (error) {
-    res.writeHead(500);
-    res.end(JSON.stringify({ error: "Internal Server Error" }));
+    default:
+      res.writeHead(405);
+      res.end(JSON.stringify({ error: "Method not allowed" }));
+      break;
   }
 }
