99dd9674a12ad2f5b7c7d157112a84f115761a67
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-13 06:02:40 -0500

    sm

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
-import { useEffect } from "react";
-
-useEffect(() => {
-  const toSource = new EventSource(
-    "https://crud-application-nine-kohl.vercel.app/api/mongo/toApiKey",
-    { withCredentials: true }
-  );
-  toSource.addEventListener("message", () => {
-    toSource.close();
-  });
-});
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
